31e1e2e252b714aee9229921bc204eece3537a7e
josephKwan <u3575182@connect.hku.hk>
2021-05-26 02:08:24 +0800

    added

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template, jsonify
+import config,csv
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from binance.enums import *
 
 app = Flask(__name__)
 
+client = Client(config.api_key,config.secret_key)
+
 @app.route("/")
 def index():
-    title = 'CoinView'
-    return render_template('index.html')
-
+    
+    #order = client.create_order(symbol='LTC',side=SIDE_BUY,type=ORDER_TYPE_LIMIT,timeInForce=TIME_IN_FORCE_GTC,quantity=100,price='0.00001')
+    title = 'myFirst'
+    info = client.get_account()
+    balances = info['balances']
+    return render_template('index.html', title=title, my_balances=balances)
 
 @app.route('/buy')
 def buy():
@@ -19,3 +27,21 @@ def sell():
 @app.route('/settings')
 def settings():
     return 'settings'
+
+@app.route('/history')
+def history():
+    candlesticks = client.get_historical_klines("ETHUSDT", client.KLINE_INTERVAL_1MINUTE,"11 hours ago UTC")
+
+    processed_candlesticks =[]
+
+    for data in candlesticks:
+        candlestick = {
+            "time":data[0]/1000,
+            "open":data[1],
+            "high":data[2],
+            "low":data[3],
+            "close":data[4]
+        }
+        processed_candlesticks.append(candlestick)
+
+    return jsonify(processed_candlesticks)
--- py_binance.py
+++ py_binance.py
@@ -1,23 +1,106 @@
-import config,csv
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+import websocket, json, pprint, talib, numpy
+import config
+from binance.client import Client
+from binance.enums import *
 
-client = Client(config.api_key, config.secret_key)
-prices = client.get_all_tickers()
-# print (prices)
+SOCKET = "wss://stream.binance.com:9443/ws/ethbusd@kline_1m"
 
-# for price in prices:
-    # print (price)
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+EMA_PERIOD = 5
+TRADE_SYMBOL = 'ETHBUSD'
+TRADE_QUANTITY = 0.15
 
-#candles = client.get_klines(symbol='BTCBUSD', interval=Client.KLINE_INTERVAL_30MINUTE)
-# candles = client.get_klines(symbol='BTCBUSD', interval=Client.KLINE_INTERVAL_15MINUTE)
-# print (len(candles))
+closes = []
+in_position = False
 
-csvfile = open('2017-2021.csv','w',newline='')
-candlestick_writer = csv.writer(csvfile,delimiter=',')
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
-klines = client.get_historical_klines("ETHUSDT", Client.KLINE_INTERVAL_5MINUTE, "19 May, 2021", "20 May, 2021")
-# for candlestick in candles:
-    # print (candlestick)
-    # candlestick_writer.writerow(candlestick)
-for kline in klines:
-    candlestick_writer.writerow(kline)
+def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
+    try:
+        print("sending order")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print(order)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return True
+
+    
+def on_open(ws):
+    print('opened connection')
+
+def on_close(ws):
+    print('closed connection')
+
+def on_message(ws, message):
+    global closes, in_position
+    
+    print('received message')
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+
+    candle = json_message['k']
+
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+    if is_candle_closed:
+        print("candle closed at {}".format(close))
+        closes.append(float(close))
+        print("closes")
+        print(closes)
+
+        if len(closes) > EMA_PERIOD:
+            np_closes = numpy.array(closes)
+            ema = talib.EMA(np_closes, EMA_PERIOD)
+            print("all EMA calculated so far")
+            print(ema)
+            last_ema = ema[-1]
+            print("the current rsi is {}".format(last_ema))
+
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print("all rsis calculated so far")
+            print(rsi)
+            last_rsi = rsi[-1]
+            print("the current rsi is {}".format(last_rsi))
+
+            if last_rsi > RSI_OVERBOUGHT and close > last_ema :
+                if in_position:
+                    print("Overbought! Sell! Sell! Sell!")
+                    # put binance sell logic here
+                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = False
+                else:
+                    print("It is overbought, but we don't own any. Nothing to do.")
+            
+            if last_rsi < RSI_OVERSOLD and close < last_ema:
+                if in_position:
+                    print("It is oversold, but you already own it, nothing to do.")
+                else:
+                    print("Oversold! Buy! Buy! Buy!")
+                    # put binance buy order logic here
+                    order_succeeded = order(SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = True
+
+            if in_position:
+                trades = client.get_my_trades(symbol='ETHBUSD')
+                last_trade = trades[-1]
+                for k,v in last_trade.items():
+                     if k == 'price':
+                        last_price = v
+                diff = (float(close)-float(last_price))/float(last_price)*100
+                if diff <= -0.01:
+                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = False
+
+                
+ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+ws.run_forever()
